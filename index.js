fead3233c0d85228a514f6937d19ef2d741bbeee
HibbaHindawi <hibba04@hotmail.se>
2022-04-26 13:55:17 +0200

    Började om från början för att göra den lite mer avancerad

--- index.js
+++ index.js
@@ -1,177 +1,27 @@
- /*Namespace*/
-  var Game      = Game      || {};
-  var Keyboard  = Keyboard  || {}; 
-  var Component = Component || {};
-
-/*Kontroller med Tangentbordet*/
-Keyboard.Keymap = {
-    37: 'left',
-    38: 'up',
-    39: 'right',
-    40: 'down'
-};
-Keyboard.ControllerEvents = function() {
-    var self      = this;
-    this.pressKey = null;
-    this.keymap   = Keyboard.Keymap;
-    
-    document.onkeydown = function(event) {
-        self.pressKey = event.which;
-    };  
-    this.getKey = function() {
-        return this.keymap[this.pressKey];
-    };
-};
-
-  /*Game Component Stage*/
- Component.Stage = function(canvas, conf) {  
-   
-    // Sets
-    this.keyEvent  = new Keyboard.ControllerEvents();
-    this.width     = canvas.width;
-    this.height    = canvas.height;
-    this.length    = [];
-    this.food      = {};
-    this.score     = 0;
-    this.direction = 'right';
-    this.conf      = {
-      cw   : 10,
-      size : 5,
-      fps  : 1000
-    };
-    
-    // Merge Conf
-    if (typeof conf == 'object') {
-      for (var key in conf) {
-        if (conf.hasOwnProperty(key)) {
-          this.conf[key] = conf[key];
-        }
-      }
+/*Polyfill*/
+(function(){ 'use strict';
+  /*Request Animation Frame*/
+  var lastTime = 0;
+  var vendors = [ 'webkit', 'moz' ];
+  for( var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x ) {
+    window.requestAnimationFrame = window[ vendors[ x ] + 'RequestAnimationFrame' ];
+    window.cancelAnimationFrame = window[ vendors[ x ] + 'CancelAnimationFrame' ] || window[ vendors[ x ] + 'CancelRequestAnimationFrame' ];
+  }
+  if( !window.requestAnimationFrame ) {
+    window.requestAnimationFrame = function( callback, element ) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max( 0, 16 - ( currTime - lastTime ) );
+      var id = window.setTimeout(
+        function() { 
+          callback( currTime + timeToCall ); 
+        }, timeToCall );
+      lastTime = currTime + timeToCall;
+      return id;
     }
-    
-  };
- /*Game Component Snake*/
-  Component.Snake = function(canvas, conf) {
-   
-    // Game Stage
-    this.stage = new Component.Stage(canvas, conf);
-    
-    // Init Snake
-    this.initSnake = function() {
-      
-      // Itaration in Snake Conf Size
-      for (var i = 0; i < this.stage.conf.size; i++) {
-        
-        // Add Snake Cells
-        this.stage.length.push({x: i, y:0});
-      }
-    };
-    
-    // Call init Snake
-    this.initSnake();
-    
-    // Init Food  
-    this.initFood = function() {
-      
-      // Add food on stage
-      this.stage.food = {
-        x: Math.round(Math.random() * (this.stage.width - this.stage.conf.cw) / this.stage.conf.cw), 
-        y: Math.round(Math.random() * (this.stage.height - this.stage.conf.cw) / this.stage.conf.cw), 
-      };
-    };
-    
-    // Init Food
-    this.initFood();
-    // Restart Stage
-   this.restart = function() {
-    this.stage.length            = [];
-    this.stage.food              = {};
-    this.stage.score             = 0;
-    this.stage.direction         = 'right';
-    this.stage.keyEvent.pressKey = null;
-    this.initSnake();
-    this.initFood();
-  };
-};
-/**
-  * Game Draw
-  */
- Game.Draw = function(context, snake) {
-   
-    // Draw Stage
-    this.drawStage = function() {
-      
-      // Check Keypress And Set Stage direction
-      var keyPress = snake.stage.keyEvent.getKey(); 
-      if (typeof(keyPress) != 'undefined') {
-        snake.stage.direction = keyPress;
-      }
-      
-      // Draw White Stage
-      context.fillStyle = "white";
-      context.fillRect(0, 0, snake.stage.width, snake.stage.height);
-      
-      // Snake Position
-      var nx = snake.stage.length[0].x;
-      var ny = snake.stage.length[0].y;
-      
-      // Add position by stage direction
-      switch (snake.stage.direction) {
-        case 'right':
-          nx++;
-          break;
-        case 'left':
-          nx--;
-          break;
-        case 'up':
-          ny--;
-          break;
-        case 'down':
-          ny++;
-          break;
-      }      
-      // Draw Snake
-      for (var i = 0; i < snake.stage.length.length; i++) {
-        var cell = snake.stage.length[i];
-        this.drawCell(cell.x, cell.y);
-      }
-      
-      // Draw Food
-      this.drawCell(snake.stage.food.x, snake.stage.food.y);
-      
-      // Draw Score
-      context.fillText('Score: ' + snake.stage.score, 5, (snake.stage.height - 5));
-    };
-    
-    // Draw Cell
-    this.drawCell = function(x, y) {
-      context.fillStyle = 'rgb(170, 170, 170)';
-      context.beginPath();
-      context.arc((x * snake.stage.conf.cw + 6), (y * snake.stage.conf.cw + 6), 4, 0, 2*Math.PI, false);    
-      context.fill();
-    };
-  };
-  
-  
-  /**
-   * Game Snake
-   */
-  Game.Snake = function(elementId, conf) {
-    
-    // Sets
-    var canvas   = document.getElementById(elementId);
-    var context  = canvas.getContext("2d");
-    var snake    = new Component.Snake(canvas, conf);
-    var gameDraw = new Game.Draw(context, snake);
-    
-    // Game Interval
-    setInterval(function() {gameDraw.drawStage();}, snake.stage.conf.fps);
-  };
-  
-  
-  /**
-   * Window Load
-   */
-  window.onload = function() {
-    var snake = new Game.Snake('stage', {fps: 100, size: 4});
-  };
+  }
+  if( !window.cancelAnimationFrame ) {
+    window.cancelAnimationFrame = function( id ) {
+      clearTimeout( id );
+    }
+  }
+})();
